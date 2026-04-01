a8b40a3c91864cdd5c0abea1106f22e1b21e93fb
heisenstack <dinaniamine@gmail.com>
2026-04-01 17:19:45 +0100

    Adding login request and register request dto for data sharing in endpoints

---
+++ src/main/java/com/lets_play/lets_play/dto/RegisterRequest.java
@@ -0,0 +1,44 @@
+package com.lets_play.lets_play.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 2, max = 50, message = "Username must be between 2 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
